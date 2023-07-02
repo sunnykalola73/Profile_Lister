e649d07600464558a7129a6f28480a1a01059141
Sunny Kalola <sunnykalola73@gmail.com>
2023-07-02 12:08:17 -0400

    Update GraphqlConnection.js
    fixes 3: updated the connector

--- src/components/GraphqlConnection.js
+++ src/components/GraphqlConnection.js
@@ -3,9 +3,9 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 export const client = new ApolloClient({
 	uri: "https://api.poc.graphql.dev.vnplatform.com/graphql",
 	headers: {
-		// TODO: This can be moved to the env file and fetch it.
-		Authorization:
-			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjYW5kaWRhdGVfbmFtZSI6IlN1bm55IiwiaXNfY2FuZGlkYXRlIjp0cnVlLCJpYXQiOjE2ODc3OTg2NzQsImV4cCI6MTY4ODMxNzA3NH0.Cp5yGasGoCD_ZKmaxFcF7eRfZ4kAUN3uzxIaQSfGFmc",
+		// TODO: This can be moved to the env file and fetched from there.
+		//We need to add personalized token below in "Authorization" field to make a Graphql call.
+		Authorization:"",
 		"Content-Type": "application/json",
 	},
 	cache: new InMemoryCache(),
